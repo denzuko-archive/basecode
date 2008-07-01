c24e7b887f29790bc367cf6ae0376c3a67f24bd2
hDc1337 <hDc1337@8a5c25fc-ea50-0410-941c-37393d7a04f2>
2008-07-01 03:58:27 +0000

    Initial Import

---
+++ include/csystem.h
@@ -0,0 +1,27 @@
+// $Id$
+
+//
+//  Mocha      
+//
+//  Created by Dwight Spencer on 2008-06-29.
+//  Copyright (c) 2008 CompuTEK Industries, LLC. All rights reserved.
+//
+
+/**
+ * @file
+ * @brief Write description of source file here for dOxygen.
+ * @brief Must use "brief" tag to generate comments for file documentation.
+ * @author Me
+ * @version 1.69
+ */
+
+#import <stdio.h>
+#import <stdlib.h>
+#import <stdarg.h>
+#import <stddef.h>
+#import <stdbool.h>
+#import <stdint.h>
+#import <unistd.h>
+#import <ctype.h>
+#import <errno.h>
+#import <string.h>
---
+++ include/mocha.h
@@ -0,0 +1,19 @@
+//
+//  Mocha
+//
+//  Created by Dwight Spencer on 2008-06-29.
+//  Copyright (c) 2008 CompuTEK Industries, LLC. All rights reserved.
+//
+
+#ifndef _MOCHA_H_
+#define _MOCHA_H_
+
+#import <objc/objc.h>
+#import <objc/Object.h>
+
+@interface Mocha: Object {
+}
+-(int) sound;
+@end
+
+#endif
